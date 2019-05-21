377c0fb6d81b45ee0a226e3483e9ebabbefb3d90
Nguyen Quan <quannguyen1456@gmail.com>
2019-05-22 00:14:04 +0700

    Định nghĩa các class và object nhiều thuộc tính với BuilderPattern

--- src/main/java/com/mycompany/helloworldapp/HelloWorldApp.java
+++ src/main/java/com/mycompany/helloworldapp/HelloWorldApp.java
@@ -19,62 +19,21 @@ import java.util.function.Predicate;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
-class Stock {
-    private String symbol;
-    private String companyName;
-    private double previosClosingPrice;
-    private double currentPrice;
 
-    Stock(String symbol, String companyName) {
-        this.symbol = symbol;
-        this.companyName = companyName;
-    }
-
-    double getChangePercent() {
-        double percent = ((currentPrice - previosClosingPrice)/previosClosingPrice) * 100;
-        DecimalFormat format = new DecimalFormat("#.##");
-        return Double.valueOf(format.format(percent));
-    }
-
-    void setPreviosClosingPrice(double newValue) {
-        previosClosingPrice = newValue;
-    }
-
-    void setCurrentPrice(double newValue) {
-        currentPrice = newValue;
-    }
-}
 
 public class HelloWorldApp {
     public static void main(String[] args) {
-        System.out.println("AbstractMap để lưu dữ liệu dạng Key-Value");
-        AbstractMap<String, Object> mapEmployee = new HashMap<String, Object>();
-        if (mapEmployee.isEmpty()) {
-            System.out.println("HashMap is empty");
-        }
-        mapEmployee.put("name", "Hoang");
-        mapEmployee.put("age", 30);
-        System.out.println("Detail employee: ");
-        System.out.println("Name: " + mapEmployee.get("name") + " Age: " + mapEmployee.get("age"));
-        System.out.println("Size: "+mapEmployee.size());
-        AbstractMap<Double, Double> mapPoints = new HashMap<Double, Double>();
-        mapPoints.put(1.0, 5.0);
-        mapPoints.put(2.0, 1.0);
-        mapPoints.put(4.0, 50.0);
-        mapPoints.put(5.0, 51.0);
-        mapPoints.forEach((key, value) -> {
-            System.out.println("Key ="+key+".Value = "+value);
-        });
-        mapPoints.remove(4.0);
-        mapPoints.forEach((key, value)->{
-            System.out.println("Key ="+key+".Value = "+value);
-        });
-
-        System.out.println("Hashcode: "+mapPoints.hashCode());
-        AbstractMap<String, Object> mapEmployee2 = new HashMap<String, Object>();
-        mapEmployee2.put("name", "Hoang");
-        mapEmployee2.put("age", 30);
-        System.out.println(mapEmployee.equals(mapEmployee2));
+        Person person = Person.Builder().salutation("Mr. ")
+                .firstName("Nguyen")
+                .middleName("Duc")
+                .lastName("Hoang")
+                .suffix("AA")
+//                .address("Số nhà 45 ngõ Mai Hương, Bạch Mai, Hà ")
+                .isFemale(false)
+                .isEmployed(true)
+                .isHomewOwner(false)
+                .build();
+        person.printDetails();
     }
 }
 
--- src/main/java/com/mycompany/helloworldapp/Person.java
+++ src/main/java/com/mycompany/helloworldapp/Person.java
@@ -13,70 +13,45 @@ import java.util.function.Function;
  * @author quan
  */
 public class Person {
-    private String name;
-    private String email;
-    private Integer age;
-    private String address;
-    
-    /**
-     *
-     * @param name
-     * @param email
-     * @param age
-     * @param address
-     */
-    public Person(String name, String email, Integer age, String address) {
-        this.name = name;
-        this.email = email;
-        this.age = age;
+    private final String salutation;//VD: Mr, Ms,
+    private final String firstName;
+    private final String middleName;
+    private final String lastName;
+    private final String suffix;
+    private final String address;
+    private final boolean isFemale;
+    private final boolean isEmployed;
+    private final boolean isHomewOwner;
+    //Giả sử có nhiều thuộc tính => nhiều hàm constructor => phức tạp
+    public Person(String salutation, String firstName,
+                  String middleName, String lastName,
+                  String suffix, String address,
+                  boolean isFemale,
+                  boolean isEmployed, boolean isHomewOwner) {
+        this.salutation = salutation;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.suffix = suffix;
         this.address = address;
+        this.isFemale = isFemale;
+        this.isEmployed = isEmployed;
+        this.isHomewOwner = isHomewOwner;
     }
-    //getter
-    public String getName() {
-        return this.name;
+
+    public void printDetails(){
+        System.out.println("salutation : "+salutation);
+        System.out.println("firstName : "+firstName);
+        System.out.println("middleName : "+middleName);
+        System.out.println("lastName : "+lastName);
+        System.out.println("suffix : "+suffix);
+        System.out.println("address : "+address);
+        System.out.println("isFemale : "+isFemale);
+        System.out.println("isEmployed : "+isEmployed);
+        System.out.println("isHomewOwner : "+isHomewOwner);
     }
-    public String getEmail() {
-        return this.email;
-    }
-    public Integer getAge() {
-        return this.age;
-    }
-    public String getAddress() {
-        return this.address;
-    }
-    //setter
-    public void setName(String name) {
-        this.name = name;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-    public void setAddress(String address) {
-        this.address = address;
-    }
-    
-    public static List<Person> generateListOfPersons() {
-        List<Person> persons = new ArrayList<Person>();
-        persons.add(new Person("Hoang", "sunlight4d@gmail.com", 35, "Mai huong, Bach Mai, Hanoi"));
-        persons.add(new Person("Huong", "huong@gmail.com", 32, "Cầu giấy, Hà "));
-        persons.add(new Person("Phương", "phuong4d@gmail.com", 30, "Nguyễn Đình Chiểu, Hanoi"));
-        persons.add(new Person("Minh", "minh4d@gmail.com", 29, "Minh Khai, Hà Nội, Việt nam"));
-        persons.add(new Person("Alexa", "alexa@gmail.com", 33, "Queensland, Australia"));
-        return persons;
-    }
-    
-    public void showDetails(){
-        System.out.println("name :"+name+
-                " email: "+email+
-                " age: "+age+
-                " address: "+address
-        );
-    }
-    
-    public void showDetails(Function<Person, String> aFunction) {
-        System.out.println(aFunction.apply(this));
+
+    public static PersonBuilder Builder() {
+        return new PersonBuilder();
     }
 }
